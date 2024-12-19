2cd6e21983332d8f59435748f1c4279fcaca2a94
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-12-20 02:41:31 +0530

    upgrade

--- src/utils/Apiresponse.js
+++ src/utils/Apiresponse.js
@@ -1,8 +1,8 @@
-class Apiresponse{
-    constructor(statusCode,data, message = "Success"){
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
-    }
+class Apiresponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
 }
