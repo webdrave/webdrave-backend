cfd91bab7bea5f9d7921109423be004a298ccd6e
Aditya Bansal <adityabansalsafe@gmail.com>
2025-01-03 15:38:23 +0530

    Change Resend to Node Mailer

--- src/utils/emailClient.ts
+++ src/utils/emailClient.ts
@@ -1,4 +1,7 @@
-import { Resend } from 'resend';
+import nodemailer from 'nodemailer';
+import client from './dbClient.js';
+
+const mails = ["khushzx8630@gmail.com", "webdrave.agency@gmail.com"];
 
 export async function sendContactEmail(
   fullname: string,
@@ -7,30 +10,55 @@ export async function sendContactEmail(
   reason: string,
   message: string
 ) {
-  const resend = new Resend();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.hostinger.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SUPPORT_EMAIL,
+      pass: process.env.SUPPORT_EMAIL_PASSWORD,
+    }
+  });
+
   try {
-    const response = await resend.emails.send({
-      from: `${fullname} <${email}>`,
-
-      //^ Update the 'to' email address with your email address
-      to: process.env.SUPPORT_EMAIL || 'agency.email.com',
-
-      subject: `Contact Form Submission: ${reason}`,
-      html: `
-        <h1>Contact Form Submission</h1>
+    const htmlMsg = `<h1>Contact Form Submission</h1>
         <p><strong>Name:</strong> ${fullname}</p>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Mobile:</strong> ${mobile}</p>
         <p><strong>Reason:</strong> ${reason}</p>
         <p><strong>Message:</strong></p>
-        <p>${message}</p>
-      `,
+        <p>${message}</p>`
+
+    mails.forEach(mail => {
+      transporter.sendMail({
+        from: process.env.SUPPORT_EMAIL,
+        to: mail,
+        subject: `Contact Form Submission: ${reason}`,
+        html: htmlMsg,
+      });
     });
-  
-    console.log("Resend API response:", response); 
-    console.log("Email sent successfully.");
+
+    const ResponseMsg = `Thank you for your submission. We will get back to you soon.`;
+
+    transporter.sendMail({
+      from: process.env.SUPPORT_EMAIL,
+      to: email,
+      subject: "Thank you ",
+      html: ResponseMsg,
+    });
+
+    await client.contact.create({
+      data: {
+        name: fullname,
+        email,
+        phone: mobile,
+        reasonOfContact : reason,
+        message,
+      },
+    });
+
   } catch (error) {
     console.error("Error from Resend API:", error);
-    throw new Error("Failed to send email.");
-  }  
+  }
 }
