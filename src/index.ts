b4400111e14bfb8b94f6c4a0cb34b214c4f5dc4d
Aditya Bansal <adityabansalsafe@gmail.com>
2025-01-10 00:29:49 +0530

    added : cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import V1Routes from './routes/v1/index.js';
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 console.log("PORT:", PORT);
 
 app.use(express.json()); 
+app.use(cors());
 
 // Use version 1 API routes
 app.use("/api/v1", V1Routes);
